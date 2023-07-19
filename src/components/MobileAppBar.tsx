193f7366355ec2f4a7cbe30f275e5632d24e4743
zake-dev <floatim00@gmail.com>
2023-07-19 18:37:48 +1000

    feat: provide search screen link button on app bar

--- src/components/MobileAppBar.tsx
+++ src/components/MobileAppBar.tsx
@@ -3,6 +3,7 @@ import {keys} from "@/constants";
 import {useSignOut} from "@/features/Auth/hooks";
 import {useAuthStore} from "@/stores";
 import {useQuery} from "@tanstack/react-query";
+import {ReactComponent as SearchIcon} from "assets/icons/search.svg";
 import {ReactComponent as SignInIcon} from "assets/icons/sign-in.svg";
 import {ReactComponent as SignOutIcon} from "assets/icons/sign-out.svg";
 import {ReactComponent as ToDashboardIcon} from "assets/icons/to-dashboard.svg";
@@ -17,20 +18,23 @@ export default function MobileAppBar() {
   const {mutate: signOut} = useSignOut();
 
   return (
-    <nav className="w-full max-w-[850px] h-16 px-6 flex justify-between items-center">
+    <nav className="w-full max-w-[850px] h-16 ps-6 pe-4 flex justify-between items-center">
       <Link to="/">
         <LogoPositive width={108} />
       </Link>
-      <li className="list-none flex items-center gap-[12px]">
-        <Link to={`/members/${profile?.id}/books`}>
+      <li className="list-none flex items-center">
+        <Link className="p-1.5" to={`/members/${profile?.id}/books`}>
           <ToDashboardIcon />
+        </Link>
+        <Link className="p-1.5" to="/books">
+          <SearchIcon />
         </Link>
         {didSignIn ? (
           <button onClick={signOut as () => void}>
             <SignOutIcon />
           </button>
         ) : (
-          <Link to="/sign-in">
+          <Link className="p-1.5" to="/sign-in">
             <SignInIcon />
           </Link>
         )}
