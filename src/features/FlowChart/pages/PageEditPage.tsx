d72b10d193f1c180ae5be82d5bf21509d7e65776
zake-dev <floatim00@gmail.com>
2023-07-27 13:31:05 +1000

    feat: page edit page UI

--- src/features/FlowChart/pages/PageEditPage.tsx
+++ src/features/FlowChart/pages/PageEditPage.tsx
@@ -1,13 +1,18 @@
 import {api} from "@/api";
-import {FlowChartAppBar, SpinningLoader} from "@/components";
+import {FlowChartAppBar, SizedBox, SpinningLoader} from "@/components";
 import {keys} from "@/constants";
-import {useAuthGuard} from "@/hooks";
+import {useAuthGuard, useTextInput} from "@/hooks";
 import {useQuery} from "@tanstack/react-query";
+import {ReactComponent as DragHandleIcon} from "assets/icons/drag-handle.svg";
+import {ReactComponent as PlusCircleIcon} from "assets/icons/plus-circle.svg";
 import {AnimatePresence, motion} from "framer-motion";
+import {FormEventHandler} from "react";
 import {useParams} from "react-router-dom";
 
 export default function PageEditPage() {
   const {bookId, pageId} = useParams();
+  const {value: title, onInput: onInputTitle} = useTextInput();
+  const {value: content, onInput: onInputContent} = useTextInput();
 
   const {
     data: page,
@@ -19,7 +24,6 @@ export default function PageEditPage() {
 
   if (!page)
     return (
-      // FIXME: 플로우차트 배경에 맞춰 bg-[#EFEFEF] 수정
       <div className="flow-chart-view bg-[#EFEFEF]">
         <FlowChartAppBar />
         <AnimatePresence mode="wait">
@@ -35,10 +39,65 @@ export default function PageEditPage() {
     );
 
   return (
-    // FIXME: 플로우차트 배경에 맞춰 bg-[#EFEFEF] 수정
     <div className="flow-chart-view bg-[#EFEFEF]">
       <FlowChartAppBar />
-      <div className="flow-chart-view-content px-6 py-0 relative flex justify-center items-center"></div>
+      <div className="flow-chart-view-content max-w-[1280px] bg-white m-4 px-6 py-4 relative flex flex-col items-stretch">
+        <div className="flex flex-col items-stretch border-t-[3px] border-black">
+          <input
+            className="h-[50px] px-6 font-medium text-[18px] placeholder:text-[#BDBDBD]"
+            value={title}
+            placeholder="페이지 제목"
+            maxLength={30}
+            onInput={onInputTitle}
+          />
+          <div className="flex-1 border-b-[3px] border-b-black flex flex-col">
+            <textarea
+              className="min-h-[600px] flex-1 border-y-2 border-[#BDBDBD] placeholder:text-[#BDBDBD] resize-none leading-7 px-6 py-3"
+              placeholder="페이지 본문"
+              value={content}
+              onInput={onInputContent as FormEventHandler}
+              maxLength={20000}
+            />
+            <p className="flex self-end items-center px-6 py-3">
+              <span className="font-bold text-[14px] me-1">{content.length.toLocaleString()}</span>
+              <span className="font-bold text-[14px] text-[#D1D1D1]">/ 20,000자</span>
+            </p>
+          </div>
+        </div>
+
+        <div className="flex flex-col gap-2 pt-4">
+          <span className="font-bold text-[18px] mb-2">선택지 만들기</span>
+          {page.choices.map(choice => (
+            <ChoiceForm />
+          ))}
+          {page.choices.length < 5 ? <AddChoiceButton /> : null}
+        </div>
+      </div>
     </div>
+  );
+}
+
+function ChoiceForm() {
+  return (
+    <div className="h-12 flex gap-1">
+      <DragHandleIcon className="self-center" />
+      <SizedBox width={4} />
+      <input
+        className="flex-1 px-4 border-2 border-black rounded-lg"
+        placeholder="선택지를 추가해주세요! (최대 100자)"
+        maxLength={100}
+      />
+      <div className="flex items-center w-[300px] px-4 bg-[#D1D1D1] border-2 border-black rounded-lg">
+        연결 페이지
+      </div>
+    </div>
+  );
+}
+
+function AddChoiceButton() {
+  return (
+    <button className="self-center flex items-center gap-2 px-4 py-2 rounded-lg hover:bg-[#EFEFEF] transition-colors font-medium text-[#858585]">
+      <PlusCircleIcon /> 선택지 추가하기
+    </button>
   );
 }
