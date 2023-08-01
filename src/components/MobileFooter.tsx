fc1488a1721e2d87069fdf771ca6ebaf30c60aa0
zake-dev <floatim00@gmail.com>
2023-08-01 15:20:03 +1000

    feat: withraw membership

--- src/components/MobileFooter.tsx
+++ src/components/MobileFooter.tsx
@@ -1,7 +1,13 @@
+import {Modal} from "@/components/Modal";
+import {useModal} from "@/hooks";
+import {useAuthStore} from "@/stores";
 import {ReactComponent as DiscordLogoIcon} from "assets/icons/discord-logo.svg";
 import {Link} from "react-router-dom";
 
 export default function MobileFooter() {
+  const {didSignIn} = useAuthStore();
+  const {isOpen, openModal, closeModal} = useModal();
+
   return (
     <div className="flex flex-col">
       <div className="px-12 py-6 bg-[#3A3A3A] flex justify-between items-center">
@@ -20,6 +26,11 @@ export default function MobileFooter() {
               support@texters.io
             </Link>
           </ul>
+          {didSignIn() ? (
+            <ul className="flex flex-col gap-[12px] font-bold text-[12px] text-[#A5A5A5]">
+              <button onClick={openModal}>회원탈퇴</button>
+            </ul>
+          ) : null}
         </div>
         <a href="https://discord.gg/wVmrzRfJ" target="_blank">
           <DiscordLogoIcon />
@@ -28,6 +39,8 @@ export default function MobileFooter() {
       <span className="py-6 border-t border-[#757575] bg-[#2D2D2D] flex justify-center items-center font-bold text-[12px] text-[#A5A5A5]">
         Copyright © Texters 2023. All Rights Reserved.
       </span>
+
+      <Modal.Withdrawal isOpen={isOpen} onRequestClose={closeModal} />
     </div>
   );
 }
--- src/components/Modal/index.ts
+++ src/components/Modal/index.ts
@@ -2,5 +2,6 @@ import Alert from "./Alert";
 import CreateBookAgreement from "./CreateBookAgreement";
 import Dialog from "./Dialog";
 import OfficialDocument from "./OfficialDocument";
+import Withdrawal from "./Withdrawal";
 
-export const Modal = {Alert, CreateBookAgreement, Dialog, OfficialDocument};
+export const Modal = {Alert, CreateBookAgreement, Dialog, OfficialDocument, Withdrawal};
